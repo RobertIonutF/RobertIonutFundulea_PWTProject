ee8dba11c0f4410be249d40227b1b457671c06e3
RobertIonutF <56986332+RobertIonutF@users.noreply.github.com>
2021-01-14 12:15:04 +0200

    Add files via upload
    Added the project itself

---
+++ src/main/java/net/fundulearobert/customburgers/CustomburgersApplication.java
@@ -0,0 +1,14 @@
+package net.fundulearobert.customburgers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomburgersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomburgersApplication.class, args);
+
+    }
+
+}
